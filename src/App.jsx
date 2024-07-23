9b1b0e7ee1c65735e4ae93b6b3e75037c1033efb
tvxxd <tavdgiridzenika1@gmail.com>
2024-07-23 20:07:18 +0400

    Initial app

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import NotesPage from "./pages/NotesPage";
+
+export default function App() {
+  return (
+    <div id="app">
+      <NotesPage />
+    </div>
+  );
+}
---
+++ src/pages/NotesPage.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+export default function NotesPage() {
+  return <div>NotesPage</div>;
+}
