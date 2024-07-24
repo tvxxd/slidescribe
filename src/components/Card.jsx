7f16702b992a73b7971013ecf6d3228a79a123da
tvxxd <tavdgiridzenika1@gmail.com>
2024-07-24 12:27:17 +0400

    style card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,10 +5,25 @@ export default function Card({ note }) {
   const colors = JSON.parse(note.colors);
   let position = JSON.parse(note.position);
   return (
-    <div className="card" style={{ backgroundColor: colors.colorBody }}>
-      {body}
+    <div
+      className="card absolute"
+      style={{
+        backgroundColor: colors.colorBody,
+        left: `${position.x}px`,
+        top: `${position.y}px`,
+      }}
+    >
+      <div
+        className="card-header card-header-border flex justify-between items-center p-[5px]"
+        style={{ backgroundColor: colors.colorHeader }}
+      ></div>
+      <div className="card-body card-body-border p-4">
+        <textarea
+          className=" bg-inherit border-0 w-full h-full resize-none text-base focus:outline-none focus:w-full focus:h-full"
+          style={{ color: colors.colorText }}
+          defaultValue={body}
+        ></textarea>
+      </div>
     </div>
   );
 }
-
-
