742894b18b5eedb72888cd1be27969d0bc09af7f
tvxxd <tavdgiridzenika1@gmail.com>
2024-07-24 13:29:43 +0400

    make card notes draggable

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,16 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Trash from "../icons/Trash";
 import { text } from "@fortawesome/fontawesome-svg-core";
 
 export default function Card({ note }) {
   const body = JSON.parse(note.body);
   const colors = JSON.parse(note.colors);
-  let position = JSON.parse(note.position);
+  const [position, setPosition] = useState(JSON.parse(note.position));
+
+  let mouseStartPosition = { x: 0, y: 0 };
+  const cardRef = useRef(null);
 
   const textAreaRef = useRef(null);
-
   useEffect(() => {
     autoGrow(textAreaRef);
   }, []);
@@ -19,8 +21,37 @@ export default function Card({ note }) {
     current.style.height = current.scrollHeight + "px";
   }
 
+  function mouseDown(e) {
+    mouseStartPosition.x = e.clientX;
+    mouseStartPosition.y = e.clientY;
+
+    document.addEventListener("mousemove", mouseMove);
+    document.addEventListener("mouseup", mouseUp);
+  }
+
+  function mouseUp() {
+    document.removeEventListener("mousemove", mouseMove);
+    document.removeEventListener("mouseup", mouseUp);
+  }
+
+  function mouseMove(e) {
+    const mouseMoveDirection = {
+      x: mouseStartPosition.x - e.clientX,
+      y: mouseStartPosition.y - e.clientY,
+    };
+
+    mouseStartPosition.x = e.clientX;
+    mouseStartPosition.y = e.clientY;
+
+    setPosition({
+      x: cardRef.current.offsetLeft - mouseMoveDirection.x,
+      y: cardRef.current.offsetTop - mouseMoveDirection.y,
+    });
+  }
+
   return (
     <div
+      ref={cardRef}
       className="card absolute"
       style={{
         backgroundColor: colors.colorBody,
@@ -31,6 +62,7 @@ export default function Card({ note }) {
       <div
         className="card-header card-header-border flex justify-between items-center p-[5px]"
         style={{ backgroundColor: colors.colorHeader }}
+        onMouseDown={mouseDown}
       >
         <Trash />
       </div>
